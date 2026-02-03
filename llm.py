b961c38f55be16303bd425937aae288a1d052e52
Jack Reamy <jack.reamy@proton.me>
2026-02-03 16:51:33 -0500

    update prompts

--- llm.py
+++ llm.py
@@ -22,7 +22,7 @@ class LLM:
     def close(self):
         self.llm.close()
 
-    def generate(self, word, length=3, dict_limit=None):
+    def generate(self, word, desc="phrase", length=3, dict_limit=None):
         seed = int(round(time.time() * 1000))
 
         self.llm.reset()
@@ -34,20 +34,21 @@ class LLM:
         other = self.dictionary.other if not dict_limit else random.sample(
             list(self.dictionary.other), min(len(self.dictionary.other), dict_limit))
 
-        form = random.choice(list(self.dictionary.forms)) if len(self.dictionary.forms) else "present tense"
+        verb_form = random.choice(list(self.dictionary.forms)) if len(
+            self.dictionary.forms) else "present active indicative"
 
-        length_rule, desc = (f"""
- - The story should be {length} sentences long.
- - Include complete sentences.
- - The story should use multiple forms of {word} if possible.
-        """, "story") if length > 1 else ("""
- - The output should be 1 brief sentence.
-        """, "sentence") if length == 1 else ("""
- - The output should be a short phrase.
-        """, "phrase")
+        if "imperative" in verb_form:
+            verb_form = f"second person {random.choice(["singular", "plural"])}"
+        elif "infinitive" not in verb_form:
+            verb_form = f"{random.choice(["first", "second", "third"])} person {random.choice(["singular", "plural"])}"
 
-        form_promt = f"The {desc} should use '{word}' in the {form}" if word in self.dictionary.verbs else f"The {desc} should use a verb in the {form}"
+        noun_form = random.choice(
+            ["nominative", "accusative", "genitive", "dative", "vocative"])
 
+        if word in self.dictionary.verbs:
+            word = self.conjugate(word, verb_form, seed=seed)
+        elif word in self.dictionary.nouns:
+            word = self.conjugate(word, noun_form, seed=seed)
 
         output = self.llm.create_chat_completion([
             {
@@ -67,9 +68,8 @@ Constraints:
  - Include only {self.dialect} word forms, no other dialect, not Modern Greek.
  - Correct all verb forms to agree with their subject.
  - Correct all agreement, spelling, and accents according to {self.dialect}.
- {length_rule.strip()}
- - Primarily use {random.choice(["first", "second", "third"])} person {random.choice(["singular", "plural"])}.
- - {form_promt}.
+ - Use {verb_form} at least once.
+ - The {desc} should be short and grammatically correct.
  - The {desc} should focus on the word '{word}'.
 """},
         ], max_tokens=length * 256, seed=seed)
@@ -77,29 +77,11 @@ Constraints:
         # .split('\n')[0]
         story = output['choices'][0]["message"]['content'].strip()
 
-        output = self.llm.create_chat_completion([
-            {
-                "role": "system",
-                "content": f"You are a helpful assistant, very adept at writing in {self.dialect}. You respond only to the task at hand and include no extra dialogue.",
-            },
-            {
-                "role": "user",
-                "content": f"""
-Task: correct the following {desc} so it contains only correct {self.dialect}.
-Constraints: 
- - Include no explanation or preamble.
- - Include only the corrected form in the output.
- - Correct all verb forms to agree with their subject.
- - Correct all agreement, spelling, and accents according to {self.dialect}.
- - Include only {self.dialect}.
- - Replace any Modern Greek with {self.dialect}.
+        story = self.correct(story, word, desc, length=length, seed=seed)
 
-Original: '{story}'
-"""},
-        ], max_tokens=length * 256, seed=seed)
+        return story, self.translate(story, desc, length=length, seed=seed)
 
-        story = output['choices'][0]["message"]['content'].strip()
-
+    def translate(self, story, desc, length=3, seed=None):
         output = self.llm.create_chat_completion([
             {
                 "role": "system",
@@ -113,11 +95,49 @@ Constraints:
  - Include only the translation in the output.
  - Do not include content that is not in the original {desc}.
  - Correct any agreement issues or incorrect spelling.
+ - Use {self.dialect} definitions when translating.
 
 Original: {story}
 """},
         ], max_tokens=length * 256, seed=seed)
 
-        translation = output['choices'][0]["message"]['content'].strip()
+        return output['choices'][0]["message"]['content'].strip()
 
-        return story, translation
+    def correct(self, story, word, desc, length=3, seed=None):
+        output = self.llm.create_chat_completion([
+            {
+                "role": "system",
+                "content": f"You are a helpful assistant, very adept at writing in {self.dialect}. You respond only to the task at hand and include no extra dialogue.",
+            },
+            {
+                "role": "user",
+                "content": f"""
+Task: correct the following {desc} so it contains only correct {self.dialect}.
+Constraints: 
+- Include no explanation or preamble.
+- Include only the corrected {desc} in the output.
+- Correct all agreement, spelling, and accents according to {self.dialect}.
+- Replace any Modern Greek with {self.dialect}.
+- The {desc} should be grammatically correct.
+- The {desc} should focus the word '{word}'.
+
+Original: '{story}'
+"""},
+        ], max_tokens=length * 256, seed=seed)
+
+        return output['choices'][0]["message"]['content'].strip()
+
+    def conjugate(self, word, form, seed=None):
+        output = self.llm.create_chat_completion([
+            {
+                "role": "system",
+                "content": f"You are a helpful assistant, very adept at writing in {self.dialect}. Your response is one word.",
+            },
+            {
+                "role": "user",
+                "content": f"""
+Task: provide the {form} of {word} in {self.dialect}. Do not incllude articles in the output.
+"""},
+        ], max_tokens=32 * 256, seed=seed)
+
+        return output['choices'][0]["message"]['content'].strip().lower()
